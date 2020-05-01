b14822710186e5a8a67057e9e42e4f3430bbe7fb
muppiry-alekhya <muppiry.alekhya49@gmail.com>
2020-05-01 17:40:09 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepositeComponent } from './deposite/deposite.component';
+import { CreateComponent } from './create/create.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
+import { FundtransferComponent } from './fundtransfer/fundtransfer.component';
+import { ShowComponent } from './show/show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepositeComponent,
+    CreateComponent,
+    WithdrawComponent,
+    FundtransferComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/deposite/deposite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deposite',
+  templateUrl: './deposite.component.html',
+  styleUrls: ['./deposite.component.css']
+})
+export class DepositeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
